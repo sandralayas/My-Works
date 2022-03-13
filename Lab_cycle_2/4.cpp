b675345ea0aa97c99f619f4868cb59871d8a940a
sandralayas <98141849+sandralayas@users.noreply.github.com>
2022-03-13 18:39:25 +0530

    Add files via upload

--- Lab_cycle_2/4.cpp
+++ Lab_cycle_2/4.cpp
@@ -10,15 +10,16 @@ class Matrix{
     int **p;
     int d1,d2;
     public:
-    Matrix(){}
+    Matrix(void){}
     Matrix(int x,int y);
-    void getelement(int i,int j,float value){
+    ~Matrix(void);
+    void getelement(int i,int j,int value){
         p[i][j]=value;
     }
     int putelement(int i,int j){
         return p[i][j];
     }
-    void output(){
+    void output(void){
         for(int i=0;i<d1;i++){
             for(int j=0;j<d2;j++){
                 cout<<putelement(i,j)<<" ";
@@ -26,7 +27,7 @@ class Matrix{
             cout<<endl;
         }
     }
-    void input(){
+    void input(void){
         cout<<"Enter the elements row by row : \n";
         int value;
         for(int i=0;i<d1;i++){
@@ -36,12 +37,17 @@ class Matrix{
             }
         }
     }
-    void matrix_add(Matrix);
-    void matrix_mult(Matrix);
-    void matrix_transpose();
-    void matrix_cofactor();
-    void matrix_determinant();
+    void operator+(Matrix&);
+    void operator*(Matrix&);
 };
+
+Matrix::~Matrix(void){
+    for(int i=0;i<d1;i++){
+        delete p[i];
+    }
+    delete p;
+    //cout<<"\nMemory relesed !!\n";
+}
 
 Matrix::Matrix(int x,int y){
     d1=x;
@@ -52,7 +58,7 @@ Matrix::Matrix(int x,int y){
     }
 }
 
-void Matrix::matrix_add(Matrix a){
+void Matrix::operator+(Matrix &a){
     for(int i=0;i<d1;i++){
         for(int j=0;j<d2;j++){
             cout<<putelement(i,j)+a.putelement(i,j)<<" ";
@@ -61,11 +67,13 @@ void Matrix::matrix_add(Matrix a){
     }
 }
 
-void Matrix::matrix_mult(Matrix a){
+void Matrix::operator*(Matrix &a){
+    int sq;
+    if(d1<=d2)sq=d1;else sq=d2;
     for(int i=0;i<d1;i++){
-        for(int j=0;j<d2;j++){
+        for(int j=0;j<a.d2;j++){
             int ans=0;
-            for(int k=0;k<d1;k++){
+            for(int k=0;k<sq;k++){
                 ans=ans+(putelement(i,k)*a.putelement(k,j));
             }
             cout<<ans<<" ";
@@ -73,47 +81,66 @@ void Matrix::matrix_mult(Matrix a){
         cout<<endl;
     }
 }
+        	
+int main(void){
+int exit,user;
+//Matrix *A,*B;
+do{
+    int m1,n1,m2,n2;
 
-void Matrix::matrix_transpose(){
-    for(int i=0;i<d1;i++){
-        for(int j=0;j<d2;j++){
-            cout<<putelement(j,i)<<" ";
-        }
-        cout<<endl;
-    }
-}
-
-void Matrix::matrix_determinant(){}
-
-main(){
-    int m,n;
     cout<<"\nEnter the size of the first matrix : \n";
-    cin>>m>>n;
-    Matrix A(m,n);
+    cin>>m1>>n1;
+    Matrix A(m1,n1);
     A.input();
-    cout<<"\nEnter the size of the second matrix : \n";
-    cin>>m>>n;
-    Matrix B(m,n);
-    B.input();
     cout<<"\nEntered first matrix :\n";
     A.output();
+
+    cout<<"\nEnter the size of the second matrix : \n";
+    cin>>m2>>n2;
+    Matrix B(m2,n2);
+    B.input();
     cout<<"\nEntered second matrix :\n";
     B.output();
-    
-/*
-    cout<<"\nTranspose of first:\n";
-    A.matrix_transpose();
 
-    cout<<"\nTranspose of second:\n";
-    B.matrix_transpose();
+int repeat=1;
+while(repeat==1){
 
+    cout<<"\n 1. Addition";
+    cout<<"\n 2. Multiplication";
+    cout<<"\n\n Enter your choice : ";cin>>user;
 
+switch(user){
+
+	case 1:
+//addition
+
+    if(m1==m2 && n1==n2){
     cout<<"\nAddition :\n";
-    A.matrix_add(B);
+    A+B;
+    }
+    else{cout<<"\nAddition not possible\n";}
 
+	break;
+
+	case 2:
+//Mutiplication
+
+    if(n2==m1){
     cout<<"\nMultiplication :\n";
-    A.matrix_mult(B);
-*/
-    cout<<"\n\nupper triangular matix\n\n";
-    A.uppertri();cout<<"\n\n";B.uppertri();
+    A*B;
+    }
+    else{cout<<"\nMultiplication not possible\n";}
+
+	break;
+
+	default:break;
 }
+cout<<"\n Enter '1' to continue with the same input : ";cin>>repeat;
+}
+
+cout<<"\n Enter '0' to stop the program : ";cin>>exit;
+}while(exit!=0);
+
+return 0;
+}
+
