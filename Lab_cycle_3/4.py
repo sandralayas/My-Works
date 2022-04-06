f762740b3210186d63141ec848c033a19eb65556
sandralayas <98141849+sandralayas@users.noreply.github.com>
2022-04-06 10:37:35 +0530

    Add files via upload

---
+++ Lab_cycle_3/4.py
@@ -0,0 +1,10 @@
+''''Design a class to store the details of a vehicle such as engine number,
+model, type, mileage, vendor, registration number, and owner name.
+Design another class that holds the details of several vehicles and
+provide functions to
+• Display the details of the collection
+• Sort the collection according to mileage
+• Add, Delete and Modify the entries from the collection
+• Store and Load the collection as a pickle file
+• Filter the result according to the attributes and export it as a
+report.'''
