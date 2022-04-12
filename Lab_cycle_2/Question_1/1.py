70120c53569fc606530504160cddd9eb897baa65
sandralayas <98141849+sandralayas@users.noreply.github.com>
2022-04-12 10:54:15 +0530

    Add files via upload

--- Lab_cycle_2/Question_1/1.py
+++ Lab_cycle_2/Question_1/1.py
@@ -11,23 +11,15 @@ def multiplication(pre_pairs,new_pairs):
     pre_pairs=temp
     return pre_pairs,new_pairs
 
-def formatting(number):
-    if number//10==0:return        '          '+str(number)
-    elif number//100==0:return    '        '+str(number)
-    elif number//1000==0:return    '      '+str(number)
-    elif number//10000==0:return    '    '+str(number)
-    elif number//100000==0:return  '    '+str(number)
-    else:return str(number)
-
 months=int(input(' Enter the number of months  : '))
-print('\n Months                    |  Pairs\n')
+print('\n Months\t\t|\t Pairs\n')
 
 pre_pairs=1
 new_pairs=1
 
 for i in range(1,months+1):
     
-    print(' Month ',formatting(i),'     |  ',formatting(pre_pairs))
+    print(' Month\t\t|\t',pre_pairs)
     pre_pairs,new_pairs=multiplication(pre_pairs,new_pairs)
 
 
