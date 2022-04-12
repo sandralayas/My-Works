49ebcbabccb258a3ed4537b706d0bf37d9583d23
sandralayas <98141849+sandralayas@users.noreply.github.com>
2022-04-12 10:51:57 +0530

    Add files via upload

--- Lab_cycle_1/Question_1/1.py
+++ Lab_cycle_1/Question_1/1.py
@@ -1,14 +1,14 @@
 # Question one ; Four digit number
 
-number=input('Enter the four digit number : ')
+number=input(' Enter the four digit number : ')
 
 
 sum=int(number[0])+int(number[1])+int(number[2])+int(number[3])# Summing up the digits
 
-print(number[0],'+',number[1],'+',number[2],'+',number[3],'=',sum)
-print(number[::-1])# Prints the reversed number
+print('\n Sum of the digits           :',number[0],'+',number[1],'+',number[2],'+',number[3],'=',sum)
+print('\n Reversed number             :',number[::-1])# Prints the reversed number
 
 Opro=int(number[0])*int(number[2])# Product of the odd position
 Epro=int(number[1])*int(number[3])# Product of the even position
 
-print(number[0],'*',number[2],'-',number[1],'*',number[3],'=',Opro-Epro)# Prints the diffrence of products
+print('\n The diffrence of products   :',number[0],'*',number[2],'-',number[1],'*',number[3],'=',Opro-Epro)# Prints the diffrence of products
