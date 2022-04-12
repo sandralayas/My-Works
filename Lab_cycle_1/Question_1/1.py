31ba64efff5203468a606a8152a764e351fa1364
sandralayas <98141849+sandralayas@users.noreply.github.com>
2022-04-12 19:15:29 +0530

    Add files via upload

--- Lab_cycle_1/Question_1/1.py
+++ Lab_cycle_1/Question_1/1.py
@@ -2,13 +2,18 @@
 
 number=input(' Enter the four digit number : ')
 
+def summing():
+	sum=int(number[0])+int(number[1])+int(number[2])+int(number[3])# Summing up the digits
+	print('\n Sum of the digits           :',number[0],'+',number[1],'+',number[2],'+',number[3],'=',sum)
 
-sum=int(number[0])+int(number[1])+int(number[2])+int(number[3])# Summing up the digits
+def reverse():
+	print('\n Reversed number             :',number[::-1])# Prints the reversed number
 
-print('\n Sum of the digits           :',number[0],'+',number[1],'+',number[2],'+',number[3],'=',sum)
-print('\n Reversed number             :',number[::-1])# Prints the reversed number
-
-Opro=int(number[0])*int(number[2])# Product of the odd position
-Epro=int(number[1])*int(number[3])# Product of the even position
-
-print('\n The diffrence of products   :',number[0],'*',number[2],'-',number[1],'*',number[3],'=',Opro-Epro)# Prints the diffrence of products
+def even_odd():
+	Opro=int(number[0])*int(number[2])# Product of the odd position
+	Epro=int(number[1])*int(number[3])# Product of the even position
+	print('\n The diffrence of products   :',number[0],'*',number[2],'-',number[1],'*',number[3],'=',Opro-Epro)
+																			# Prints the diffrence of products
+summing()
+reverse()
+even_odd()
