0deea71444ed47c5780e7fbc59cd0fc6fac7ee23
sandralayas <98141849+sandralayas@users.noreply.github.com>
2022-05-20 14:44:26 +0530

    Add files via upload

---
+++ 2.java
@@ -0,0 +1,12 @@
+class Addition {
+    public static void main(String[] args) {    //input is string
+        
+        int variable1,variable2,sum;            //random variables
+        
+        variable1=100;
+        variable2=200;
+        
+        sum=variable1+variable2;
+        System.out.println(sum);                //print the sum
+    }
+}
