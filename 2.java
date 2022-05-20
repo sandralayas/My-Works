ac5a93ccafb5d4cb3e36f2b648ba3d0b367b37c4
sandralayas <98141849+sandralayas@users.noreply.github.com>
2022-05-20 15:29:57 +0530

    Add files via upload

--- 2.java
+++ 2.java
@@ -1,12 +1,20 @@
+import java.util.Scanner;
+
 class Addition {
     public static void main(String[] args) {    //input is string
         
         int variable1,variable2,sum;            //random variables
         
-        variable1=100;
-        variable2=200;
+        Scanner input=new Scanner(System.in);
+        
+        System.out.print("\n Enter the first variable : ");
+        variable1=input.nextInt();
+        
+        System.out.print("\n Enter the second variable : ");
+        variable2=input.nextInt();
         
         sum=variable1+variable2;
+        System.out.print("\n The sum is : ");
         System.out.println(sum);                //print the sum
     }
 }
