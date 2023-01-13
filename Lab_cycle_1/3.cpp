7564c1f3fe27ca3268953da41e64e4514d5eb6c3
sandralayas <98141849+sandralayas@users.noreply.github.com>
2023-01-13 16:21:28 +0530

    Update 3.cpp

--- Lab_cycle_1/3.cpp
+++ Lab_cycle_1/3.cpp
@@ -34,6 +34,7 @@ void Graph::DFS(int v)
 
 int main()
 {
+	/*
 	Graph g(6);
 	g.addEdge(0, 1);
 	g.addEdge(0, 2);
@@ -47,6 +48,45 @@ int main()
 			" (starting from vertex 2) \n";
 
 	g.DFS(2);
+	*/
+	int input;
 
+	cout << "\n Enter the number of nodes : ";
+	cin >> input;
+	int size = input;
+	Graph g(size);
+
+	int choice;
+	do
+	{
+		cout << "\n 1. Add an edge"
+			 << "\n 2. Output BFS"
+			 << "\n\n Enter your choice : ";
+		cin >> input;
+
+		choice = input;
+		if (choice == 1)
+		{
+			int x, y;
+			cout << "\n Enter the parent node : ";
+			cin >> input;
+			x = input;
+			cout << " Enter its child node : ";
+			cin >> input;
+			y = input;
+			g.addEdge(x, y);
+		}
+
+		if (choice == 2)
+		{
+			cout << "\n Enter the source node : ";
+			cin >> input;
+			int source = input;
+			cout << "Following is Breadth First Traversal starting from vertex " << source << endl;
+			g.DFS(source);
+		}
+	} while (choice != 2);
+
+	cout << endl;
 	return 0;
 }
