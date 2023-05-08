dcb0c4d39fd0ccb8e72672641307c732722fe954
sandralayas <98141849+sandralayas@users.noreply.github.com>
2023-05-08 15:51:15 +0530

    Update Transportation_problem.py

--- Operations_research/Transportation_problem.py
+++ Operations_research/Transportation_problem.py
@@ -1,12 +1,34 @@
-grid = [[3, 1, 7, 4], [2, 6, 5, 9], [8, 3, 3, 2]]  # table
-supply = [300, 400, 500]  # supply
-demand = [250, 350, 400, 200]  # demand
-INF = 10**3
+grid = []
+row = []
+supply = []
+demand = []
+
+rownum = int(  ("Enter the number of rows") )
+
+colnum = int(  ("Enter the number of columns") )
+
+for i in range (rownum):
+    print("Enter row "+str(i+1))
+    for j in range(colnum):
+        row[j] = int(input ("\n"))
+    grid[i] = row
+
+print("Enter the supply row")   
+for i in range(rownum):
+    supply[i] = int(input ("\n"))
+
+print("Enter the demand row")
+for i in range(colnum):
+    demand[i] = int(input("\n"))
+
+#grid = [[3, 1, 7, 4], [2, 6, 5, 9], [8, 3, 3, 2]]  # table
+#supply = [300, 400, 500]  # supply
+#demand = [250, 350, 400, 200]  # demand
+INF = 10**rownum
 n = len(grid)
 m = len(grid[0])
 ans = 0
- 
-# hepler function for finding the row difference and the column difference
+
 def findDiff(grid):
     rowDiff = []
     colDiff = []
@@ -24,44 +46,41 @@ def findDiff(grid):
         colDiff.append(arr[1]-arr[0])
     return rowDiff, colDiff
  
- 
-# loop runs until both the demand and the supply is exhausted
 while max(supply) != 0 or max(demand) != 0:
-    # finding the row and col difference
+
     row, col = findDiff(grid)
-    # finding the maxiumum element in row difference array
+
     maxi1 = max(row)
-    # finding the maxiumum element in col difference array
+
     maxi2 = max(col)
  
-    # if the row diff max element is greater than or equal to col diff max element
     if(maxi1 >= maxi2):
         for ind, val in enumerate(row):
             if(val == maxi1):
-                # finding the minimum element in grid index where the maximum was found in the row difference
+
                 mini1 = min(grid[ind])
                 for ind2, val2 in enumerate(grid[ind]):
                     if(val2 == mini1):
-                        # calculating the min of supply and demand in that row and col
+                        
                         mini2 = min(supply[ind], demand[ind2])
                         ans += mini2 * mini1
-                        # subtracting the min from the supply and demand
-                        supply[ind] -= mini2
+
+                 supply[ind] -= mini2
                         demand[ind2] -= mini2
-                        # if demand is smaller then the entire col is assigned max value so that the col is eliminated for the next iteration
+
                         if(demand[ind2] == 0):
                             for r in range(n):
                                 grid[r][ind2] = INF
-                        # if supply is smaller then the entire row is assigned max value so that the row is eliminated for the next iteration
+
                         else:
                             grid[ind] = [INF for i in range(m)]
                         break
                 break
-    # if the row diff max element is greater than col diff max element
+
     else:
         for ind, val in enumerate(col):
             if(val == maxi2):
-                # finding the minimum element in grid index where the maximum was found in the col difference
+
                 mini1 = INF
                 for j in range(n):
                     mini1 = min(mini1, grid[j][ind])
@@ -69,18 +88,18 @@ while max(supply) != 0 or max(demand) != 0:
                 for ind2 in range(n):
                     val2 = grid[ind2][ind]
                     if val2 == mini1:
-                        # calculating the min of supply and demand in that row and col
+
                         mini2 = min(supply[ind2], demand[ind])
                         ans += mini2 * mini1
-                        # subtracting the min from the supply and demand
+
                         supply[ind2] -= mini2
                         demand[ind] -= mini2
-                        # if demand is smaller then the entire col is assigned max value so that the col is eliminated for the next iteration
+
                         if(demand[ind] == 0):
                             for r in range(n):
                                 grid[r][ind] = INF
-                        # if supply is smaller then the entire row is assigned max value so that the row is eliminated for the next iteration
-                        else:
+
+                            else:
                             grid[ind2] = [INF for i in range(m)]
                         break
                 break
