7fafc4e1fa1cf8684c76d8b7a3c7c6f421326edf
sandralayas <98141849+sandralayas@users.noreply.github.com>
2023-05-08 18:04:27 +0530

    Update Transportation_problem.py

--- Operations_research/Transportation_problem.py
+++ Operations_research/Transportation_problem.py
@@ -1,109 +1,113 @@
 
-rownum = int( input ("Enter the number of rows : ") )
+rownum = int( input ("\n Enter the number of rows : ") )
 
-colnum = int( input ("Enter the number of columns : ") )
+colnum = int( input ("\n Enter the number of columns : ") )
 
 grid = list()
-row = list()
+rows = list()
 supply = list()
 demand = list()
 
 
 for i in range (rownum):
-    print("Enter row "+str(i+1))
+    print("\n Enter row",i+1,"\n")
     for j in range(colnum):
-        row.append( int(input ("\n")))
-    grid.append( row )
-    row = []
+        rows.append( int(input (" ")))
+    grid.append( rows )
+    rows = []
 
-print("Enter the supply row")   
-for i in range(rownum):
-    supply[i] = int(input ("\n"))
+print("\n Enter the supply row\n")   
+for i in range(rownum):supply.append( int(input (" ")) )
 
-print("Enter the demand row")
-for i in range(colnum):
-    demand[i] = int(input("\n"))
+print("\n Enter the demand row\n")
+for j in range(colnum):demand.append( int(input(" ")) )
 
-#grid = [[3, 1, 7, 4], [2, 6, 5, 9], [8, 3, 3, 2]]  # table
-#supply = [300, 400, 500]  # supply
-#demand = [250, 350, 400, 200]  # demand
-INF = 10**rownum
+print('\n The grig is :',grid)
+print(' The supply is :',supply)
+print(' The demand is :',demand)
+
+INF = 10**3
 n = len(grid)
 m = len(grid[0])
 ans = 0
 
+
 def findDiff(grid):
-    rowDiff = []
-    colDiff = []
-    for i in range(len(grid)):
-        arr = grid[i][:]
-        arr.sort()
-        rowDiff.append(arr[1]-arr[0])
-    col = 0
-    while col < len(grid[0]):
-        arr = []
-        for i in range(len(grid)):
-            arr.append(grid[i][col])
-        arr.sort()
-        col += 1
-        colDiff.append(arr[1]-arr[0])
-    return rowDiff, colDiff
- 
+	rowDiff = []
+	colDiff = []
+	for i in range(len(grid)):
+		arr = grid[i][:]
+		arr.sort()
+		rowDiff.append(arr[1]-arr[0])
+	col = 0
+	while col < len(grid[0]):
+		arr = []
+		for i in range(len(grid)):
+			arr.append(grid[i][col])
+		arr.sort()
+		col += 1
+		colDiff.append(arr[1]-arr[0])
+	return rowDiff, colDiff
+
+
+
 while max(supply) != 0 or max(demand) != 0:
+	
+	row, col = findDiff(grid)
+	
+	maxi1 = max(row)
+	
+	maxi2 = max(col)
 
-    row, col = findDiff(grid)
+	
+	if(maxi1 >= maxi2):
+		for ind, val in enumerate(row):
+			if(val == maxi1):
+				
+				mini1 = min(grid[ind])
+				for ind2, val2 in enumerate(grid[ind]):
+					if(val2 == mini1):
+						
+						mini2 = min(supply[ind], demand[ind2])
+						ans += mini2 * mini1
+						
+						supply[ind] -= mini2
+						demand[ind2] -= mini2
+						
+						if(demand[ind2] == 0):
+							for r in range(n):
+								grid[r][ind2] = INF
+						
+						else:
+							grid[ind] = [INF for i in range(m)]
+						break
+				break
+	
+	else:
+		for ind, val in enumerate(col):
+			if(val == maxi2):
+				
+				mini1 = INF
+				for j in range(n):
+					mini1 = min(mini1, grid[j][ind])
 
-    maxi1 = max(row)
+				for ind2 in range(n):
+					val2 = grid[ind2][ind]
+					if val2 == mini1:
+						
+						mini2 = min(supply[ind2], demand[ind])
+						ans += mini2 * mini1
+						
+						supply[ind2] -= mini2
+						demand[ind] -= mini2
+						
+						if(demand[ind] == 0):
+							for r in range(n):
+								grid[r][ind] = INF
+						
+						else:
+							grid[ind2] = [INF for i in range(m)]
+						break
+				break
 
-    maxi2 = max(col)
- 
-    if(maxi1 >= maxi2):
-        for ind, val in enumerate(row):
-            if(val == maxi1):
-
-                mini1 = min(grid[ind])
-                for ind2, val2 in enumerate(grid[ind]):
-                    if(val2 == mini1):
-                        
-                        mini2 = min(supply[ind], demand[ind2])
-                        ans += mini2 * mini1
-                        supply[ind] -= mini2
-                        demand[ind2] -= mini2
-
-                        if(demand[ind2] == 0):
-                            for r in range(n):
-                                grid[r][ind2] = INF
-
-                        else:
-                            grid[ind] = [INF for i in range(m)]
-                        break
-                break
-
-    else:
-        for ind, val in enumerate(col):
-            if(val == maxi2):
-
-                mini1 = INF
-                for j in range(n):
-                    mini1 = min(mini1, grid[j][ind])
- 
-                for ind2 in range(n):
-                    val2 = grid[ind2][ind]
-                    if val2 == mini1:
-
-                        mini2 = min(supply[ind2], demand[ind])
-                        ans += mini2 * mini1
-
-                        supply[ind2] -= mini2
-                        demand[ind] -= mini2
-
-                        if(demand[ind] == 0):
-                            for r in range(n):
-                                grid[r][ind] = INF
-
-                            else:
-                                grid[ind2] = [INF for i in range(m)]
-                        break
-                break
- 
-print("The basic feasible solution is ", ans)
+print("\n The basic feasible solution is ", ans)
