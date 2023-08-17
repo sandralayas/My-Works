9b6b428bb1ef32d8966f487eb5b8e910ed16a966
sandralayas <98141849+sandralayas@users.noreply.github.com>
2023-08-17 13:43:59 +0530

    Update Travelling_Salesman_Problem.py

--- Operations_research/Travelling_Salesman_Problem.py
+++ Operations_research/Travelling_Salesman_Problem.py
@@ -48,7 +48,7 @@ if __name__ == "__main__":
     nodes=int(input("Enter the number of nodes : "))
     graph=[]
     for i in range(nodes):graph.append(inputArray(nodes))
-	# graph = [[0, 10, 15, 20], [10, 0, 35, 25],
-	# 		[15, 35, 0, 30], [20, 25, 30, 0]]
+    # graph = [[0, 10, 15, 20], [10, 0, 35, 25],[15, 35, 0, 30], [20, 25, 30, 0]]
     s = 0
-    print(travellingSalesmanProblem(graph, s))
+    for i in graph:print(i)
+    print("\nThe optimum value is :",travellingSalesmanProblem(graph, s))
