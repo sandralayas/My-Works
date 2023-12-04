c25e3914515aa908ef3fd5a848d16145b86c4f34
sandralayas <98141849+sandralayas@users.noreply.github.com>
2023-12-04 12:46:54 +0530

    Update Q4.R

--- Lab Cycle 2/Q4.R
+++ Lab Cycle 2/Q4.R
@@ -1,3 +1,9 @@
+# Q5:  Time Series Visualization. Develop an R program to 
+# create a time series plot using real-world data. (https://www.kaggle.com/datasets/niketchauhan/covid-19-time-series-data)
+# Plot the time series object  and add a title and label. 
+# Customize the plot to show trendlines and seasonal patterns. Discuss the insights gained from the visualization.
+
+
 covid <- read.csv('C:/Users/cusat/Documents/Bigdata/dataset/covid.csv')
 print(covid)
 
