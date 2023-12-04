464a05640c6af4f46355084bc151f481f4430ad2
sandralayas <98141849+sandralayas@users.noreply.github.com>
2023-12-04 12:44:18 +0530

    Update Q3.R

--- Lab Cycle 2/Q3.R
+++ Lab Cycle 2/Q3.R
@@ -1,3 +1,7 @@
+# Q3: Create a box plot of the horsepower (hp) in the mtcars dataset. 
+# Use different shapes to represent the number of gears (gear). Add appropriate title, 
+# labels, and legend to the plot. Identify and label any outliers on the plot.
+
 # Load the ggplot2 library
 library(ggplot2)
 
