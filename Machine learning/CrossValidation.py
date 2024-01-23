c8d043341e52cd8ffcd74a4945b9e4e5454a1849
sandralayas <98141849+sandralayas@users.noreply.github.com>
2024-01-23 14:51:23 +0530

    Update CrossValidation.py

--- Machine learning/CrossValidation.py
+++ Machine learning/CrossValidation.py
@@ -1,1 +1,71 @@
+# Hold out Method
 
+def load_csv(filepath):
+    data =  []
+    col = []
+    checkcol = False
+    with open(filepath) as f:
+        for val in f.readlines():
+            val = val.replace("\n","")
+            val = val.split(',')
+            if checkcol is False:
+                col = val
+                checkcol = True
+            else:
+                data.append(val)
+    df = pd.DataFrame(data=data, columns=col)
+    return df
+
+# Leave p-out Method
+
+def load_csv(filepath):
+    data =  []
+    col = []
+    checkcol = False
+    with open(filepath) as f:
+        for val in f.readlines():
+            val = val.replace("\n","")
+            val = val.split(',')
+            if checkcol is False:
+                col = val
+                checkcol = True
+            else:
+                data.append(val)
+    df = pd.DataFrame(data=data, columns=col)
+    return df
+
+# Leave 1-out Method
+
+def load_csv(filepath):
+    data =  []
+    col = []
+    checkcol = False
+    with open(filepath) as f:
+        for val in f.readlines():
+            val = val.replace("\n","")
+            val = val.split(',')
+            if checkcol is False:
+                col = val
+                checkcol = True
+            else:
+                data.append(val)
+    df = pd.DataFrame(data=data, columns=col)
+    return df
+
+# KFold Method
+
+from sklearn import datasets
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import KFold, cross_val_score
+
+X, y = datasets.load_iris(return_X_y=True)
+
+clf = DecisionTreeClassifier(random_state=42)
+
+k_folds = KFold(n_splits = 5)
+
+scores = cross_val_score(clf, X, y, cv = k_folds)
+
+print("Cross Validation Scores: ", scores)
+print("Average CV Score: ", scores.mean())
+print("Number of CV Scores used in Average: ", len(scores))
